b6fe5c26f4e658d3be353d5ba5d9d8baf9c52adf
5hyn3 <hse9795@gmail.com>
2017-12-30 03:30:16 +0900

    Unified quotation in single quotes

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -5,20 +5,20 @@ var $ = require('jquery')
 var crypto = require('crypto')
 
 var NODES = Array(
-    "alice2.nem.ninja:7890",
-    "alice3.nem.ninja:7890",
-    "alice4.nem.ninja:7890",
-    "alice5.nem.ninja:7890",
-    "alice6.nem.ninja:7890",
-    "alice7.nem.ninja:7890"
+    'alice2.nem.ninja:7890',
+    'alice3.nem.ninja:7890',
+    'alice4.nem.ninja:7890',
+    'alice5.nem.ninja:7890',
+    'alice6.nem.ninja:7890',
+    'alice7.nem.ninja:7890'
 );
 
 var algorithms = {
-    1: "md5",
-    2: "sha1",
-    3: "sha256",
-    8: "sha3-256",
-    9: "sha3-512"
+    1: 'md5',
+    2: 'sha1',
+    3: 'sha256',
+    8: 'sha3-256',
+    9: 'sha3-512'
 }
 
 var getTimeStamp = function (time) {
@@ -33,7 +33,7 @@ function string_to_buffer(src) {
 }
 
 function failed() {
-    document.getElementById('status').textContent = "監査に失敗しました"
+    document.getElementById('status').textContent = '監査に失敗しました'
 }
 
 function check(url) {
@@ -43,7 +43,7 @@ function check(url) {
 
     xhr.onload = function (e) {
         var pageBinary = this.response
-        var filename = url.match(".+/(.+?)([\?#;].*)?$")[1]
+        var filename = url.match('.+/(.+?)([\?#;].*)?$')[1]
         var message = filename.match(/.*%20--%20Apostille%20TX%20([0-9a-f]{64})%20--%20Date%20\d{4}-\d{1,2}-\d{1,2}/)
 
         var hash = crypto.createHash('sha256')
@@ -57,7 +57,7 @@ function check(url) {
         var txhash = message[1]
         var getEndpoint = function () {
             var target_node = NODES[Math.floor(Math.random() * NODES.length)];
-            return "http://" + target_node + "/transaction/get?hash=" + txhash;
+            return 'http://' + target_node + '/transaction/get?hash=' + txhash;
         }
         var sendAjax = function () {
             $.ajax({ url: getEndpoint(), type: 'GET' }).then(
@@ -69,8 +69,8 @@ function check(url) {
         }
 
         var checkTransaction = function (res) {
-            var payload = res["transaction"]["message"]["payload"]
-            if (payload.slice(2, 9) != "4e54590") {
+            var payload = res['transaction']['message']['payload']
+            if (payload.slice(2, 9) != '4e54590') {
                 failed()
             }
             var algorithm = payload.slice(9, 10)
@@ -78,9 +78,9 @@ function check(url) {
             hash.update(new Buffer(pageBinary))
             var pagesHash = hash.digest('hex')
             if (payload.slice(10) == pagesHash) {
-                document.getElementById('status').textContent = "監査に成功しました"
-                var date = new Date(getTimeStamp(res["transaction"]["timeStamp"]))
-                document.getElementById('timestamp').textContent = "TimeStamp"
+                document.getElementById('status').textContent = '監査に成功しました'
+                var date = new Date(getTimeStamp(res['transaction']['timeStamp']))
+                document.getElementById('timestamp').textContent = 'TimeStamp'
                 document.getElementById('time').textContent = date.toUTCString()
             } else {
                 failed()
