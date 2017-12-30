e85e657d292ec1a480d312c8113e4f3aeae10e84
5hyn3 <hse9795@gmail.com>
2017-12-30 19:58:09 +0900

    summarize processing to access NimAPI

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -38,24 +38,18 @@ function failed() {
     $('#status').text('監査に失敗しました');
 }
 
-function setOwnerAddress(publicKey) {
-    console.log(publicKey)
+function sendRequestNimAPI(path, func) {
     const getEndpoint = function () {
         const target_node = NODES[Math.floor(Math.random() * NODES.length)];
-        return 'http://' + target_node + '/account/get/from-public-key?publicKey=' + publicKey;
+        return 'http://' + target_node + path;
     }
     const sendAjax = function () {
         $.ajax({ url: getEndpoint(), type: 'GET' }).then(
-            function (res) { checkTransaction(res) },
+            function (res) { func(res) },
             function (res) {
                 sendAjax();
             }
         )
-    };
-
-    const checkTransaction = function (res) {
-        console.log(res)
-        $('#address').text('Owner:' + res['account']['address'])
     };
     sendAjax();
 }
@@ -68,26 +62,14 @@ function check(url) {
     xhr.onload = function (e) {
         const pageBinary = this.response;
         const filename = url.match('.+/(.+?)([\?#;].*)?$')[1];
-        const message = filename.match(/.*%20--%20Apostille%20TX%20([0-9a-f]{64})%20--%20Date%20\d{4}-\d{1,2}-\d{1,2}/)
+        const message = filename.match(/.*%20--%20Apostille%20TX%20([0-9a-f]{64})%20--%20Date%20\d{4}-\d{1,2}-\d{1,2}/);
 
         if (message == null) {
             failed();
             return;
         }
         const txhash = message[1];
-        const getEndpoint = function () {
-            const target_node = NODES[Math.floor(Math.random() * NODES.length)];
-            return 'http://' + target_node + '/transaction/get?hash=' + txhash;
-        }
-        const sendAjax = function () {
-            $.ajax({ url: getEndpoint(), type: 'GET' }).then(
-                function (res) { checkTransaction(res) },
-                function (res) {
-                    sendAjax();
-                }
-            )
-        };
-
+        const getTransactionPath = '/transaction/get?hash=' + txhash;
         const checkTransaction = function (res) {
             const payload = res['transaction']['message']['payload']
             if (payload.slice(0, 9) != messagesFixedValue) {
@@ -101,12 +83,16 @@ function check(url) {
                 const date = new Date(getTimeStamp(res['transaction']['timeStamp']));
                 $('#status').text('監査に成功しました');
                 $('#timestamp').text('TimeStamp:' + date.toUTCString());
-                setOwnerAddress(res['transaction']['signer'])
+                const getAccountFromPublicKeyPath = '/account/get/from-public-key?publicKey=' + res['transaction']['signer'];
+                const setOwnerAddress = function (res) {
+                    $('#address').text('Owner:' + res['account']['address']);
+                };
+                sendRequestNimAPI(getAccountFromPublicKeyPath, setOwnerAddress);
             } else {
                 failed();
             }
         };
-        sendAjax();
+        sendRequestNimAPI(getTransactionPath, checkTransaction);
     }
 
     xhr.send();
