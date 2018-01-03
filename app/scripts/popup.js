db85d05d98f7d356769dab59649622ededaf949d
5hyn3 <hse9795@gmail.com>
2018-01-03 20:54:53 +0900

    add nem_audit

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -4,41 +4,7 @@
 'use strict';
 
 const $ = require('jquery');
-
-function nemAudit(transaction, pageBinary) {
-    const crypto = require('crypto');
-
-    const algorithms = {
-        1: 'md5',
-        2: 'sha1',
-        3: 'sha256',
-        8: 'sha3-256',
-        9: 'sha3-512'
-    };
-
-    const messagesFixedValue = 'fe4e54590';
-    const payload = transaction['transaction']['message']['payload']
-
-    if (payload.slice(0, 9) != messagesFixedValue) {
-        return false;
-    }
-
-    const algorithm = payload.slice(9, 10);
-    const hash = crypto.createHash(algorithms[algorithm]);
-    hash.update(new Buffer(pageBinary));
-    const pagesHash = hash.digest('hex');
-
-    if (payload.slice(10) == pagesHash) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function getTimeStamp(time) {
-    const NEM_EPOCH = Date.UTC(2015, 2, 29, 0, 6, 25, 0);
-    return Math.floor((time * 1000 + NEM_EPOCH));
-}
+const nemAudit = require('./nem_audit');
 
 function string_to_buffer(src) {
     return (new Uint16Array([].map.call(src, function (c) {
@@ -48,33 +14,6 @@ function string_to_buffer(src) {
 
 function failed() {
     $('#status').text('監査に失敗しました');
-}
-
-function sendRequestNimAPI(path, func) {
-    const NODES = Array(
-        'alice2.nem.ninja:7890',
-        'alice3.nem.ninja:7890',
-        'alice4.nem.ninja:7890',
-        'alice5.nem.ninja:7890',
-        'alice6.nem.ninja:7890',
-        'alice7.nem.ninja:7890'
-    );
-
-    const getEndpoint = function () {
-        const target_node = NODES[Math.floor(Math.random() * NODES.length)];
-        return 'http://' + target_node + path;
-    };
-
-    const sendAjax = function () {
-        $.ajax({ url: getEndpoint(), type: 'GET' }).then(
-            function (res) { func(res) },
-            function (res) {
-                sendAjax();
-            }
-        )
-    };
-
-    sendAjax();
 }
 
 function audit(url) {
@@ -96,8 +35,8 @@ function audit(url) {
         const getTransactionPath = '/transaction/get?hash=' + txhash;
 
         const checkTransaction = function (res) {
-            if (nemAudit(res, pageBinary)) {
-                const date = new Date(getTimeStamp(res['transaction']['timeStamp']));
+            if (nemAudit.audit(res, pageBinary)) {
+                const date = new Date(nemAudit.getTimeStamp(res['transaction']['timeStamp']));
                 $('#status').text('監査に成功しました');
                 $('#timestamp').text('TimeStamp:' + date.toUTCString());
                 const getAccountFromPublicKeyPath = '/account/get/from-public-key?publicKey=' + res['transaction']['signer'];
@@ -106,12 +45,12 @@ function audit(url) {
                     $('#address').text('Owner:' + res['account']['address']);
                 };
 
-                sendRequestNimAPI(getAccountFromPublicKeyPath, setOwnerAddress);
+                nemAudit.sendRequestNimAPI(getAccountFromPublicKeyPath, setOwnerAddress);
             } else {
                 failed();
             }
         };
-        sendRequestNimAPI(getTransactionPath, checkTransaction);
+        nemAudit.sendRequestNimAPI(getTransactionPath, checkTransaction);
     }
 
     xhr.send();
